7c81bb10e20d5e1ad1c1061e47d22f653f150a7a
Chris Jones <chrisdjones15@gmail.com>
2013-05-24 14:25:11 -0500

    first submission

---
+++ Makefile
@@ -0,0 +1,39 @@
+#Change this to point to your boost installation
+BOOST_INCLUDES := -I/Users/cdj/src/cms/software/FWLiteVersions/osx108_amd64_gcc472/external/boost/1.50.0-cms4/include
+BOOST_LIB_PATH := -L/Users/cdj/src/cms/software/FWLiteVersions/osx108_amd64_gcc472/external/boost/1.50.0-cms4/lib
+#BOOST_INCLUDES := -I$(BOOST_INC)
+CXXOTHERFLAGS :=
+UNAME:=$(shell uname -s)
+#UNAME:=Linux
+TBB_INCLUDES :=-I/Users/cdj/src/cms/software/FWLiteVersions/osx108_amd64_gcc472/external/tbb/41_20120718oss/include
+TBB_LIB_PATH :=-L/Users/cdj/src/cms/software/FWLiteVersions/osx108_amd64_gcc472/external/tbb/41_20120718oss/lib
+#TBB_LIB := $(TBB_LIB_PATH) -ltbb_debug
+ifeq ($(UNAME), Linux)
+BOOST_INCLUDES := -I/uscms_data/d2/cdj/build/multicore/dispatch/install/include
+#change the following to to point to where you installed tbb
+TBB_INCLUDES := -I/uscms_data/d2/cdj/build/multicore/tbb/tbb40_20120408oss/include
+#TBB_LIB_PATH := -L/uscms_data/d2/cdj/build/multicore/tbb/tbb40_20120408oss//build/linux_intel64_gcc_cc4.7.0_libc2.5_kernel2.6.18_debug
+TBB_LIB_PATH := -L/uscms_data/d2/cdj/build/multicore/tbb/tbb40_20120408oss//build/linux_intel64_gcc_cc4.7.0_libc2.5_kernel2.6.18_release
+CXXOTHERFLAGS := -D__USE_XOPEN2K8
+endif
+TBB_LIB := $(TBB_LIB_PATH) -ltbb
+BOOST_LIB := $(BOOST_LIB_PATH) -lboost_thread
+CXX=g++
+CPPFLAGS=$(TBB_INCLUDES) $(BOOST_INCLUDES) $(CPPUNIT_INCLUDES) 
+CXXFLAGS=-O3 -g -std=c++0x -fPIC -pthread $(CXXOTHERFLAGS)
+#CXXFLAGS=-g -std=c++0x -fPIC -pthread
+LDFLAGS= -std=c++0x -pthread
+LINKER=g++
+
+all: transitionTest
+
+objects := $(patsubst %.cc,%.o,$(wildcard *.cc))
+
+
+transitionTest: $(objects)
+	$(LINKER) $+ $(LDFLAGS) $(TBB_LIB) -o $@
+
+clean:
+	rm -f *.o 
+	rm -f transitionTest
+	rm -f $(objects)
---
+++ Source.cc
@@ -0,0 +1,105 @@
+// -*- C++ -*-
+//
+// Package:     Package
+// Class  :     Source
+// 
+// Implementation:
+//     [Notes on implementation]
+//
+// Original Author:  Chris Jones
+//         Created:  Fri, 24 May 2013 16:05:37 GMT
+// $Id$
+//
+
+// system include files
+
+// user include files
+#include "Source.h"
+#include "Event.h"
+#include "Run.h"
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+Source::Source(unsigned int iNRuns, unsigned int iNEventsPerRun, unsigned int iNEvents):
+m_nextTransition{iNEvents==0? kStop: kRun},
+m_nextRunNumber{1},
+m_nextEventNumber{0},
+m_nEventsSeen{0},
+m_nRuns{iNRuns},
+m_nEventsPerRun{iNEventsPerRun},
+m_nEvents{iNEvents}
+{
+}
+
+// Source::Source(const Source& rhs)
+// {
+//    // do actual copying here;
+// }
+
+//Source::~Source()
+//{
+//}
+
+//
+// assignment operators
+//
+// const Source& Source::operator=(const Source& rhs)
+// {
+//   //An exception safe implementation is
+//   Source temp(rhs);
+//   swap(rhs);
+//
+//   return *this;
+// }
+
+//
+// member functions
+//
+void 
+Source::finishTransition()
+{
+   if(kRun == m_nextTransition) {
+      m_nextTransition = kEvent;
+      ++m_nextEventNumber;
+   }
+   if(kEvent == m_nextTransition) {
+      ++m_nEventsSeen;
+      if(m_nEventsSeen == m_nEvents) {
+         m_nextTransition = kStop;
+      }
+      else if(m_nEventsSeen % m_nEventsPerRun == 0) {
+         m_nextTransition = kRun;
+         ++m_nextRunNumber;
+      } else {
+         ++m_nextEventNumber;
+      }
+   }
+}
+
+
+void Source::gotoNextEvent(Event& iEvent) {
+ iEvent.setNumber( {nextRunsNumber(),nextEventsNumber()});
+ finishTransition();
+}
+
+void Source::gotoNextRun(Run& iRun) {
+ iRun.set(nextRunsNumber(),0);
+ finishTransition();
+}
+
+//
+// const member functions
+//
+
+//
+// static member functions
+//
