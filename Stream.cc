7e113087a28b5418c3c90e76cf1b350fb400be4d
Chris Jones <chrisdjones15@gmail.com>
2013-05-30 13:35:19 -0500

    added counters to check on parallel running

--- Stream.cc
+++ Stream.cc
@@ -13,6 +13,8 @@
 
 // system include files
 #include <unistd.h>
+#include <atomic>
+#include <cstdio>
 #include "tbb/task.h"
 
 // user include files
@@ -26,6 +28,9 @@
 //
 // static data member definitions
 //
+static std::atomic<unsigned int> s_simultaneousEvents{0};
+static std::atomic<unsigned int> s_simultaneousBeginRuns{0};
+static std::atomic<unsigned int> s_simultaneousEndRuns{0};
 
 //
 // constructors and destructor
@@ -46,26 +51,35 @@ m_id(iID),m_state(kInitialized),m_runTransitionID(0),m_run(0)
 
 void 
 Stream::processBeginRun(Run const* iRun) {
+   unsigned int n = ++s_simultaneousBeginRuns;
+   printf("#streamBeginRuns %u\n",n);
    m_runTransitionID = iRun->transitionID();
    m_run = iRun;
    m_state = kBeginRun;
    usleep(100);
+   --s_simultaneousBeginRuns;
 }
 void 
 Stream::processEvent() {
+   unsigned int n = ++s_simultaneousEvents;
+   printf("#events %u\n",n);
    m_state = kEvent;
    usleep(1000);
+   --s_simultaneousEvents;
 }
 //will decrement iDoneTask and if reach 0 will spawn it
 // this allows global end run to be called once all streams
 // are done with a 
 void 
 Stream::processEndRun(tbb::task* iDoneTask) {
+   unsigned int n = ++s_simultaneousEndRuns;
+   printf("#streamEndRuns %u\n",n);
    m_state=kEndRun;
    usleep(100);
    if(0==iDoneTask->decrement_ref_count()) {
       tbb::task::spawn(*iDoneTask);
    }
+   --s_simultaneousEndRuns;
 }
 
 void 
